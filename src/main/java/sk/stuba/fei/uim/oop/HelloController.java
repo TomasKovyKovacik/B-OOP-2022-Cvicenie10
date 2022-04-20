e7d3de0ccbf5a60cc50e0cb5fb93fba8569e429a
TomasKovyKovacik <tomas_kovacik@stuba.sk>
2022-04-20 16:11:21 +0200

    post body

--- src/main/java/sk/stuba/fei/uim/oop/HelloController.java
+++ src/main/java/sk/stuba/fei/uim/oop/HelloController.java
@@ -1,9 +1,6 @@
 package sk.stuba.fei.uim.oop;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
@@ -30,5 +27,8 @@ public class HelloController {
         return "hello " + name;
     }
 
-
+    @PostMapping("/body")
+    public String body(@RequestBody HelloRequestBody body) {
+        return "hello " + body.getName();
+    }
 }
---
+++ src/main/java/sk/stuba/fei/uim/oop/HelloRequestBody.java
@@ -0,0 +1,8 @@
+package sk.stuba.fei.uim.oop;
+
+import lombok.Getter;
+
+@Getter
+public class HelloRequestBody {
+    private String name;
+}
