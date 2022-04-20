120fa8793d272449fa3a82efdf76471c83a6820d
TomasKovyKovacik <tomas_kovacik@stuba.sk>
2022-04-20 15:54:03 +0200

    add first endpoint

---
+++ src/main/java/sk/stuba/fei/uim/oop/HelloController.java
@@ -0,0 +1,13 @@
+package sk.stuba.fei.uim.oop;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello world";
+    }
+}
---
+++ src/main/java/sk/stuba/fei/uim/oop/HelloWorld.java
@@ -0,0 +1,10 @@
+package sk.stuba.fei.uim.oop;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloWorld {
+    public HelloWorld() {
+        System.out.println("Hello World");
+    }
+}
